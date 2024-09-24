4bd3d07a9c4a655e54f31d625655b9b59d1031b4
zcomert <zafer.comert@hotmail.com>
2024-09-24 11:02:07 +0300

    Hello World project has been created.

---
+++ ch_01_hello/Program.cs
@@ -0,0 +1,36 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+string message = "Hello world.";
+
+app.MapGet("/hello", () =>
+{
+    return new Response(message);
+})
+.WithName("hello")
+.WithOpenApi();
+
+app.UseHttpsRedirection();
+
+app.Run();
+
+class Response
+{
+    public Response(string msg)
+    {
+        Message = msg;
+    }
+    public String? Message { get; set; }
+    public DateTime? Date => DateTime.Now;
+}
+
